4c3a5d5ce30a61a83291019a23d52a113840f6c1
w_953 <w_953421@163.com>
2021-07-14 17:14:08 +0800

    添加文件

---
+++ src/main/java/com/atguigu/git/HelloGet.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+/**
+ * Created by zwq on 2021/7/14 16:57
+ */
+public class HelloGet {
+    public static void main(String[] args) {
+        System.out.println("hellow get!");
+    }
+}
